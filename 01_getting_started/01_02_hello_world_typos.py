d7d85c9a37496637b6f5280370718beff9e7d2a2
Sumin Ma <115057649+ma-sumin@users.noreply.github.com>
2024-08-30 08:30:57 -0400

    Update 01_02_hello_world_typos.py

--- 01_getting_started/01_02_hello_world_typos.py
+++ 01_getting_started/01_02_hello_world_typos.py
@@ -1,8 +1,8 @@
 #1-2. Hello World Typos
 
 # a Python script with a typo
-print('Hello World!') # --> no errors
-# print{"Hello World!"} # --> causes an error
+# print('Hello World!') # --> no errors
+print{"Hello World!"} # --> causes an error
 
 # troubleshooting the errors
 print("Hello World!")
