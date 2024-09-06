c57edd51b4596dd35ba5a6db7dc124d9fd437fac
ma-sumin <amnaen1209@naver.com>
2024-09-05 23:05:48 -0400

    commit

--- 02_variables_and_data_types/02_11_adding_comments.py
+++ 02_variables_and_data_types/02_11_adding_comments.py
@@ -1,12 +1,17 @@
 #2-11. Adding Comments
 
-#write two of the programs and add at least one comment to each
+'''
+I wrote commetns on all of the assignment files for this week,
+but I also wrote two of the programs and added at least one comment to each
+'''
 
 '''program_01
 written by: sumin
 date: 4th Sep, 2024
 description: asking for the user's name and greeting them
 '''
+#title
+print("#prgram_01_greeting")
 #assign a question to a variable
 q_name = "What is your name? "
 #assign the user's input to a variable 
@@ -23,12 +28,14 @@ written by: sumin
 date: 4th Sep, 2024
 description: calculator for Fahrenheit to Celsius
 '''
+#title
+print("#prgram_02_convert_temperature")
 #assign a question for degrees in Fahrenheit to a variable
 q_fah = "How cold is it in Fahrenheit? "
 #assign the user's input to a variable
 fah = int(input(q_fah))
-#convert it to Celsius
-cal_f_to_c = 5/9 * (fah - 32)
+#convert it to Celsius by rounding to 2 decimal places
+cal_f_to_c = round(5/9 * (fah - 32),2)
 #assign a message to tell the user the degrees in Celsius
 message = f"It's {cal_f_to_c} degrees Celsius."
 #print the message
--- 02_variables_and_data_types/02_12_zen_of_python.py
+++ 02_variables_and_data_types/02_12_zen_of_python.py
@@ -2,3 +2,13 @@
 
 #enter import this and skim through them
 import this
+
+#write what I did and what you learned from this information by using comments in your code
+'''
+I've learned that Python has a clear philosophy guiding how we should code.
+It was a helpful reminder for me to focus on writing clean, efficient, and readable code.
+Python emphasizes simplicity and clarity, which not only makes the code easier for others
+to understand but also helps prevent errors in the long run. By following these principles,
+I can create code that's not only functional but also elegant, making it easier to maintain
+and scale as projects grow.
+'''
