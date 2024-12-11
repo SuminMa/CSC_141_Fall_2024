ee7add09523489c39f7fb373c65ff9197600ccc0
Sumin Ma <115057649+ma-sumin@users.noreply.github.com>
2024-12-11 04:22:42 -0500

    Add files via upload

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -1,20 +1,22 @@
 import pygame
+from pygame.sprite import Sprite
 
-class Ship:
+class Ship(Sprite):
     """A class to manage the ship."""
 
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
+        super().__init__()
         self.screen = ai_game.screen
         self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect.
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('images/ship2.png')
         self.rect = self.image.get_rect()
 
-        # Places a ship on the left side of the screen.
-        self.rect.midleft = self.screen_rect.midleft
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
 
         # Store a float for the ship's exact horizontal position.
         self.x = float(self.rect.x)
@@ -50,3 +52,8 @@ class Ship:
     def blitme(self):
         """Draw the ship at its current location."""
         self.screen.blit(self.image, self.rect)
+
+    def center_ship(self):
+        """Center the ship on the screen."""
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
